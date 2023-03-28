2d15c9879511332f9d1fbdb83dbb770b26e91b8a
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-03-27 23:17:11 -0400

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,22 @@
+import nextMDX from '@next/mdx'
+import remarkGfm from 'remark-gfm'
+import rehypePrism from '@mapbox/rehype-prism'
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx'],
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true,
   },
-  target: 'serverless',
-  webpack: (config) => {
-    // Add your webpack configuration here
-    return config
-  },
 }
 
-module.exports = nextConfig
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkGfm],
+    rehypePlugins: [rehypePrism],
+  },
+})
+
+export default withMDX(nextConfig)
